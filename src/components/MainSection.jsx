2d85888a35a6c60cff2f03e635cb8080ddf542e3
NightOwlLab <ivantjandra18@gmail.com>
2025-05-21 15:29:27 +0700

    fix: video size & carousel interval

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,21 +2,35 @@ import Picture from '/profile picture/ivan.png';
 import Carousel1 from '/carousel/Carousel1-compressed.mp4';
 import Carousel2 from '/carousel/Carousel2-compressed.mp4';
 import Carousel3 from '/carousel/Carousel3-compressed.mp4';
+import { useEffect } from 'react';
 
 const MainSection = () => {
+
+  useEffect(() => {
+    const carouselEl = document.querySelector('#carouselExampleInterval');
+    if (carouselEl) {
+      const carousel = bootstrap.Carousel.getOrCreateInstance(carouselEl, {
+        interval: 3000,
+        ride: 'carousel',
+        pause: false
+      });
+      carousel.cycle();
+    }
+  }, []);
+
   return (
     <section id="main-section" className='z-1' style={{ height: '100vh'}} >
       <div className="position-relative w-100 h-100 z-2">
         <div id="carouselExampleInterval" className="carousel slide carousel-fade h-100 z-1" data-bs-ride="carousel">
           <div className="carousel-inner h-100">
-            <div className="carousel-item h-100 active" data-bs-interval="3000" loading="lazy">
-              <video loading="lazy" src={Carousel1} autoPlay loop playsInline className="d-block w-100 object-fit-cover h-100"/>
+            <div className="carousel-item h-100 active" data-bs-interval="3000">
+              <video src={Carousel1} muted autoPlay loop playsInline className="d-block w-100 object-fit-cover h-100"/>
             </div>
-            <div className="carousel-item h-100" data-bs-interval="3000" loading="lazy">
-              <video loading="lazy" src={Carousel2} autoPlay loop playsInline className="d-block w-100 object-fit-cover h-100"/>
+            <div className="carousel-item h-100" data-bs-interval="3000">
+              <video src={Carousel2} muted autoPlay loop playsInline className="d-block w-100 object-fit-cover h-100"/>
             </div>
-            <div className="carousel-item h-100" data-bs-interval="3000" loading="lazy">
-              <video loading="lazy" src={Carousel3} autoPlay loop playsInline className="d-block w-100 object-fit-cover h-100"/>
+            <div className="carousel-item h-100" data-bs-interval="3000">
+              <video src={Carousel3} muted autoPlay loop playsInline className="d-block w-100 object-fit-cover h-100"/>
             </div>
           </div>
         </div>
